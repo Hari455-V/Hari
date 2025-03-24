9cbf61187360db2a83f068fa273c3ecd4e7ce672
Hari455-V <harimech455@gmail.com>
2025-03-24 18:47:49 +0530

    new code added

---
+++ src/omr/branch/GitClass.java
@@ -0,0 +1,20 @@
+package omr.branch;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class GitClass {
+	public void haran() {
+	 System.out.println(" Name:Hari");
+		
+
+	}
+	public static void main(String[] args) {
+		GitClass g=new GitClass();
+		g.haran();
+	}
+
+	
+			
+
+}
